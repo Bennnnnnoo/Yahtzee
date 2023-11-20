20ad9e832011f32254d0b65d03ce55269eb6d252
Benno Pixley <bcpixsley@perse.co.uk>
2023-11-20 12:41:54 +0000

    markov algorithm probability calulations

--- markovchaintest.py
+++ markovchaintest.py
@@ -18,8 +18,16 @@ def count_dice_values(dice_list):
         else:
             print(f"Illegal dice value: {value}")
     
-    state =  max(dice_count.values())
-    return states[state]
+    dicestates = {
+        1:states[dice_count[1]],
+        2:states[dice_count[2]],
+        3:states[dice_count[3]],
+        4:states[dice_count[4]],
+        5:states[dice_count[5]],
+        6:states[dice_count[6]]
+        }
+    
+    return dicestates
 
    
 
@@ -27,6 +35,7 @@ def count_dice_values(dice_list):
         
        # 
 states = {
+    0: 0,
     1: [1,0,0,0,0], # 1 matching dice
     2: [0,1,0,0,0], # 2 matching dice
     3: [0,0,1,0,0], # 3 matching dice
@@ -37,10 +46,10 @@ states = {
   
 probabilities_graph = [
     [120/1296, 900/1296, 250/1296, 25/1296, 1/1296], # 1 matching dice
-    [None, 120/216, 80/216, 15/216, 1/216], # 2 matching dice
-    [None, None, 25/36, 10/36, 1/36], # 3 matching dice
-    [None, None, None, 5/6, 1/6],   # 4 matching dice
-    [None, None, None, None, 1]    # 5 matching dice
+    [0, 120/216, 80/216, 15/216, 1/216], # 2 matching dice
+    [0, 0, 25/36, 10/36, 1/36], # 3 matching dice
+    [0, 0, 0, 5/6, 1/6],   # 4 matching dice
+    [0, 0, 0, 0, 1]    # 5 matching dice
 ]
 
 
@@ -50,23 +59,26 @@ def reroll(dice, keep):
             dice[i] = random.randint(1, 6)
     return dice
 
-expected_values = [calculate_probability(dice, state) for state in states.values()]
 
-def calculate_probability(dice, state):
+
+def calculate_probability(state):
     probabilities = np.multiply(state, probabilities_graph)
 
-    S1 = probabilities[0] # probability of getting to state 1
+    '''S1 = probabilities[0] # probability of getting to state 1
     S2 = probabilities[1] # probability of getting to state 2
     S3 = probabilities[2] # probability of getting to state 3
     S4 = probabilities[3] # probability of getting to state 4
-    S5 = probabilities[4] # probability of getting to state 5
+    S5 = probabilities[4] # probability of getting to state 5'''
      
+    return [probabilities]
 
-    return
+def run_alg():
+    dstates = count_dice_values(dice)
+    for key,state in dstates.items():
+        dstates[key] = calculate_probability(state)
+    return dstates
 
 
-
-
-print(count_dice_values(dice))
+print(run_alg())
 
     
