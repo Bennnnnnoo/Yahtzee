03f7e40d99d669543e5aaf0b09d945f66dab5672
Benno Pixley <bcpixsley@perse.co.uk>
2023-12-10 13:56:53 +0000

    Add GUI functionality and fix reroll prompt

--- UI.py
+++ UI.py
@@ -1,5 +1,5 @@
 from Yahtzeegamecode import Game, Player, Scorecard, Dice
-import random, math, PySimpleGUI as psg
+import random, math, PySimpleGUI as psg, time
 
 
 
@@ -48,7 +48,7 @@ class Terminal:
             
             
         def get_reroll_choice(self):
-            reroll_list = input("Enter the dice indexes you want to keep: ").split(',')
+            reroll_list = input("Enter the dice indexes you want to reroll: ").split(',')
             reroll_list = [int(i) for i in reroll_list]
             return reroll_list
             
@@ -75,11 +75,16 @@ class Terminal:
             return choice  
             
         def tround(self):
+
+            self.game.roundnum += 1
+
             print("Round " + str(self.game.roundnum))
             for player in self.game.players:
                 self.turn(player)
                 
         def turn(self,player):
+
+            
             print(player.name + "'s turn")
             self.game.dice.roll()
             print("Your dice are: " + str(self.game.dice.get_dice()))
@@ -134,7 +139,11 @@ class GUI:
 
 
     def run(self):
-        psg.popup("Welcome to Yahtzee!")
+        psg.theme("DarkGreen1")
+
+        
+
+
         layout = [[psg.Text("Welcome to Yahtzee!")],
                   [psg.T("")],
                   [psg.Button("Start new Game", size = (50, 5) )],
@@ -150,7 +159,9 @@ class GUI:
         while True:
             event, values = window.read()
             if event == "Start new Game":
+                window.close()
                 self.newgame()
+                
             elif event == "Load Game":
                 self.loadgame()
             elif event == "Leaderboard":
@@ -177,38 +188,107 @@ class GUI:
         while self.game.roundnum < 13*len(self.game.players):
             self.roundgui()
 
+        psg.popup("Game Over!")
+
+        
+
+        self.run()
+        
+
 
 
     def roundgui(self):
-        psg.popup("Round " + str(self.roundnum))
+        self.game.roundnum += 1
+        psg.popup("Round " + str(self.game.roundnum))
         for player in self.game.players:
-            self.turngui()
+            self.turngui(player)
 
 
-    def turngui(self):
-        psg.popup("Player " + str(self.player) + "'s turn")
-        self.dice = Dice()
-        self.dice.roll()
-        self.dicegui = self.dice.get_dice()
-        psg.popup("Your dice are: " + str(self.dicegui))
+    def turngui(self, player):
+
+        # makes scorecard dict to 2d array for easy table reading
+
+        scorecardlist = []
+        for key in player.scorecard.keylist:
+            scorecardlist.append([key, player.scorecard.scorecard[key]])
+
+        # table initialisation
+        playerscoretable = psg.Table(values = scorecardlist, headings = ["Category", "Score"], num_rows=(14), auto_size_columns=True, justification='c')
+        
+        # variable initialisaion
+        self.game.rerolls = 0
+        self.game.dice.roll()
+        
+        # layout
+        layout = [
+            [psg.Text("Round " + str(self.game.roundnum))],
+            [psg.Text(str(player.name) + "'s turn")],
+            [playerscoretable],
+            [psg.Text('Your dice are: ' + str(self.game.dice.get_dice()), key = 'dice')],
+            [psg.Text('Enter the dice you want to reroll: ')],[psg.Input(key='reroll')],
+            [psg.Button("Reroll")],
+            [psg.Input(key='score_category')],
+            [psg.Button("Score")],
+
+
+
+
+       ]
+        
+
+
+
+
+
+        window = psg.Window("Yahtzee", layout, size=(700, 500), element_justification='c')
+        while True:
+            event,values = window.read()
+            if event == "Reroll":
+                
+                if self.game.rerolls < self.game.REROLLS:
+                    self.game.rerolls += 1
+                    self.game.dice.reroll(values['reroll'].split(','))
+                    window['reroll'].update('')
+                    window['dice'].update('Your dice are: ' + str(self.game.dice.get_dice()))
+                else:
+                    psg.popup("You have used all your rerolls!")
+            elif event == "Score":
+                player.scorecard.score_roll(self.game.dice, int(values['score_category']))
+                window['Score'].update(visible=False)
+                scorecardlist.clear()
+                for key in player.scorecard.keylist:
+                    scorecardlist.append([key, player.scorecard.scorecard[key]])
+                
+                playerscoretable.update(scorecardlist)
+                time.sleep(1)
+                
+                break
+            elif event == psg.WIN_CLOSED:
+                break
+            
+        window.close()
+
+
+
+    ''' rolls = 0
+        psg.popup(str(player.name) + "'s turn")
+        self.game.dice = Dice()
+        self.game.dice.roll()
+        
+        psg.popup("Your dice are: " + str(self.game.dice.get_dice()))
         self.rolls = 0
-        while self.rolls < 2:
-            self.rolls += 1
-            self.keep = psg.popup_get_text("Enter the dice you want to keep: ")
-            self.dice.roll(self.keep)
-            self.dicegui = self.dice.get_dice()
-            psg.popup("Your dice are: " + str(self.dicegui))
+        while rolls < 2:
+            rolls += 1
+            rerolls = psg.popup_get_text("Enter the dice you want to keep: ")
+            self.dice.roll(rerolls)
+            psg.popup("Your dice are: " + str(self.game.dice.get_dice()))
         self.category = psg.popup_get_text("Enter the category you want to score in: ")
-        self.player.scorecard.score(self.category, self.dice)
-        self.roundnum += 1
+        player.scorecard.score_roll(self.game.dice, int(self.catgory))
+        self.roundnum += 1 '''
 
 
             
-        psg.popup("Game Over!")
-
-
-
-if __name__ == "__main__":    # Main function
+def __get_game_mode():
     game_mode = input("Play in terminal or GUI? (t/g): ")
     if game_mode == "t":
         game = Terminal()
@@ -216,6 +296,15 @@ if __name__ == "__main__":    # Main function
     elif game_mode == "g":
         game = GUI()
         game.run()
+    else:
+        print("Invalid game mode")
+        __get_game_mode()
+
+
+if __name__ == "__main__":    # Main function
+    __get_game_mode()
+
+    
 
 
 
